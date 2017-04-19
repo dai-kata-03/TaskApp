2234872e117eb6034e4e8bef01f4a05ba20be6ea
Daisuke <siq009@yahoo.ca>
2017-04-19 11:55:10 -0400

    Lesson6 - Chapter 4まで。

--- app/src/main/java/jp/techacademy/katahara/daisuke/taskapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/katahara/daisuke/taskapp/MainActivity.java
@@ -4,19 +4,23 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView; // ListViewのメンバ変数。
+    private TaskAdapter mTaskAdapter; // TaskAdapterのメンバ変数。
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,27 +30,41 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // ListViewの設定
+        mTaskAdapter = new TaskAdapter(MainActivity.this); // TaskAdapterのインスタンス。
+        mListView = (ListView) findViewById(R.id.listView1);
+
+        // ListViewをタップした時の処理
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 入力・編集する画面に遷移させる
+            }
+        });
+
+        // ListViewを長押しした時の処理
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                // タスクを削除する
+                return true;
+            }
+        });
+
+        reloadListView(); // ページのリロード（再読み込み）を行う。
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void reloadListView() {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // 後でTaskクラスに変更する
+        List<String> taskList = new ArrayList<String>(); // ArrayListクラスのインスタンス。
+        taskList.add("aaa");
+        taskList.add("bbb");
+        taskList.add("ccc");
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        mTaskAdapter.setTaskList(taskList);
+        mListView.setAdapter(mTaskAdapter);
+        mTaskAdapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/jp/techacademy/katahara/daisuke/taskapp/TaskAdapter.java
@@ -0,0 +1,59 @@
+package jp.techacademy.katahara.daisuke.taskapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Daisuke on 2017/04/19.
+ */
+
+public class TaskAdapter extends BaseAdapter {
+
+    private LayoutInflater mLayoutInflater; // 他のxmlリソースのViewを取り扱うための仕組みであるLayoutInflateをメンバ変数で定義。
+
+    private List<String> mTaskList; // リストの定義（メンバ変数）。
+
+    public TaskAdapter(Context context) {
+        mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void setTaskList(List<String> taskList) {
+        mTaskList = taskList;
+    }
+
+    @Override
+    public int getCount() {
+        return mTaskList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mTaskList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = mLayoutInflater.inflate(android.R.layout.simple_list_item_2, null);
+        }
+
+        TextView textView1 = (TextView) convertView.findViewById(android.R.id.text1);
+        TextView textView2 = (TextView) convertView.findViewById(android.R.id.text2);
+
+        // 後でTaskクラスから情報を取得するように変更する。
+        textView1.setText(mTaskList.get(position));
+
+        return convertView;
+    }
+}
